b998e6d2a61e6ab6cdd681bd33d1fd8461e79893
Suélio <suelio.pontes@signainfo.com.br>
2026-07-17 18:29:35 -0300

    feat: Implementando Domain Events para Customer

---
+++ src/domain/@shared/event/event-dispatcher-customer.spec.ts
@@ -0,0 +1,95 @@
+﻿import CustomerAddressChangedEvent from "../../customer/event/customer-address-changed.event";
+import CustomerCreatedEvent from "../../customer/event/customer-created.event";
+import EnviaConsoleLogHandler from "../../customer/event/handler/envia-console-log-handler";
+import EnviaConsoleLog1Handler from "../../customer/event/handler/envia-console-log1-handler";
+import EnviaConsoleLog2Handler from "../../customer/event/handler/envia-console-log2-handler";
+import EventDispatcher from "./event-dispatcher";
+
+describe("Domain events tests", () => {
+  it("should register an event handler CustomerCreatedEvent", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new EnviaConsoleLog1Handler();
+        const eventHandler2 = new EnviaConsoleLog2Handler();
+
+        eventDispatcher.register("CustomerCreatedEvent", eventHandler);
+        eventDispatcher.register("CustomerCreatedEvent", eventHandler2);
+
+        expect(
+        eventDispatcher.getEventHandlers["CustomerCreatedEvent"]
+        ).toBeDefined();
+        expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"].length).toBe(
+        2
+        );
+
+        expect(
+        eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]
+        ).toMatchObject(eventHandler);
+        expect(
+        eventDispatcher.getEventHandlers["CustomerCreatedEvent"][1]
+        ).toMatchObject(eventHandler2); 
+
+    });
+
+    it("should unregister an event handler CustomerCreatedEvent", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new EnviaConsoleLog1Handler();
+        const eventHandler2 = new EnviaConsoleLog2Handler();
+
+        eventDispatcher.register("CustomerCreatedEvent", eventHandler);
+        eventDispatcher.register("CustomerCreatedEvent", eventHandler2);
+
+        eventDispatcher.unregister("CustomerCreatedEvent", eventHandler);
+        eventDispatcher.unregister("CustomerCreatedEvent", eventHandler2);
+
+        expect(
+        eventDispatcher.getEventHandlers["CustomerCreatedEvent"]
+        ).toBeDefined();
+        expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"].length).toBe(
+        0
+        );
+    });
+
+    it("should notify all event handlers CustomerCreatedEvent when new CustomerCreatedEvent is triggered",() => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new EnviaConsoleLog1Handler();
+        const eventHandler2 = new EnviaConsoleLog2Handler();
+
+        const spyEventHandler = jest.spyOn(eventHandler, "handle");
+        const spyEventHandler2 = jest.spyOn(eventHandler2, "handle");
+
+        eventDispatcher.register("CustomerCreatedEvent", eventHandler);
+        eventDispatcher.register("CustomerCreatedEvent", eventHandler2);
+
+        const customerCreatedEvent = new CustomerCreatedEvent({
+            id: "1",
+            name: "Customer 1",
+            address: "Address 1"
+        });
+
+        eventDispatcher.notify(customerCreatedEvent);
+
+        expect(spyEventHandler).toHaveBeenCalled();
+        expect(spyEventHandler2).toHaveBeenCalled();
+    });
+
+
+    it("should notify all event g=handlers CustomerAddressChangedEvent when new CustomerAddressChangedEvent is triggered",() => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new EnviaConsoleLogHandler();
+
+        const spyEventHandler = jest.spyOn(eventHandler, "handle");
+
+        eventDispatcher.register("CustomerAddressChangedEvent", eventHandler);
+
+        const customerAddressChangedEvent = new CustomerAddressChangedEvent({
+            id: "1",
+            name: "Customer 1",
+            address: "Address 2"
+        });
+
+        eventDispatcher.notify(customerAddressChangedEvent);
+
+        expect(spyEventHandler).toHaveBeenCalled();
+    });
+    
+});
---
+++ src/domain/customer/event/handler/envia-console-log-handler.ts
@@ -0,0 +1,8 @@
+﻿import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
+import CustomerAddressChangedEvent from "../customer-address-changed.event";
+
+export default class EnviaConsoleLogHandler implements EventHandlerInterface<CustomerAddressChangedEvent> {
+  handle(event: CustomerAddressChangedEvent): void {
+    console.log(`Endereço do cliente: ${event.eventData.id}, ${event.eventData.name} alterado para: ${event.eventData.address}`);
+  }
+}
